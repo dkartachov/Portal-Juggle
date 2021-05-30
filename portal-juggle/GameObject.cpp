3ef79aa586c8cf81ec3eb59666dfb9977b0de89a
Denis Kartachov <kartachovd@gmail.com>
2021-05-30 14:56:13 -0400

    Add gameobject class files

--- portal-juggle/GameObject.cpp
+++ portal-juggle/GameObject.cpp
@@ -1,1 +1,37 @@
 #include "GameObject.h"
+
+GameObject::GameObject() {
+	w = h = 20;
+	position.x = position.y = 0;
+	velocity.x = velocity.y = 0;
+
+	srcRect.w = srcRect.h = srcRect.x = srcRect.y = 0;
+
+	destRect.w = w;
+	destRect.h = h;
+	destRect.x = position.x;
+	destRect.y = position.y;
+}
+
+void GameObject::Size(int width, int height) {
+	w = width;
+	h = height;
+}
+
+void GameObject::AddSprite(const char* filename) {
+	surface = IMG_Load(filename); 
+	srcRect.w = surface->w;
+	srcRect.h = surface->h;
+	texture = SDL_CreateTextureFromSurface(Game::renderer, surface);
+}
+
+void GameObject::Render() {
+	SDL_RenderCopy(Game::renderer, texture, &srcRect, &destRect);
+}
+
+void GameObject::Update() {
+	destRect.w = w;
+	destRect.h = h;
+	destRect.x = position.x;
+	destRect.y = position.y;
+}
--- portal-juggle/GameObject.h
+++ portal-juggle/GameObject.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "Game.h"
+#include "Vector2D.h"
+
+class GameObject {
+public:
+	int w, h;
+	Vector2D position, velocity;
+	SDL_Rect srcRect, destRect;
+	GameObject();
+	void Size(int width, int height);
+	void AddSprite(const char* filename);
+	void Render();
+	void Update();
+private:
+	SDL_Surface* surface;
+	SDL_Texture* texture;
+};
